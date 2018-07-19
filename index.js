5f3417cb562532bab12baad5d372050e8637a119
Thales Pereira <thalesfp@gmail.com>
2018-07-19 14:37:30 -0300

    Add fuzzy search

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const inquirer = require('inquirer');
 const { spawnSync } = require('child_process');
+const fuzzy = require('fuzzy');
 
 const isTest = process.env.NODE_ENV === 'test';
+
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
 const logger = message => !isTest && process.stdout.write(`${message}\n`);
 
 const shellCommand = (command, params) => {
   const { status, stdout, stderr } = spawnSync(command, params);
+
+  logger([command, ...params].join(' '));
 
   if (status !== 0) {
     logger(`Error: ${stderr.toString()}`);
@@ -16,19 +21,29 @@ const shellCommand = (command, params) => {
   return stdout.toString();
 };
 
-const getBranches = (executeCmd = shellCommand) => {
-  const branches = executeCmd('git', ['branch']);
+const getBranches = (executeCmd = shellCommand, input) => {
+  return new Promise((resolve) => {
+    const branches = executeCmd('git', ['branch']);
 
-  if (branches === '') {
-    logger('Error: This repository has no branches');
-    process.exit(1);
-  }
+    if (branches === '') {
+      logger('Error: This repository has no branches');
+      process.exit(1);
+    }
 
-  return branches
-    .replace('*', '')
-    .split('\n')
-    .filter(e => e !== '')
-    .map(e => e.trim());
+    let branchList = branches
+      .replace('*', '')
+      .split('\n')
+      .filter(e => e !== '')
+      .map(e => e.trim());
+
+    if (input) {
+      branchList = fuzzy
+        .filter(input, branchList)
+        .map(e => e.string);
+    }
+
+    resolve(branchList);
+  });
 };
 
 const doCheckout = branch => logger(shellCommand('git', ['checkout', branch]));
@@ -37,14 +52,14 @@ if (!isTest) {
   inquirer
     .prompt([
       {
-        type: 'list',
+        type: 'autocomplete',
         name: 'branch',
         message: 'Please choose a branch to checkout:',
-        choices: getBranches(shellCommand),
-        pageSize: 10,
+        source: (answersSoFar, input) => getBranches(shellCommand, input),
       },
     ])
-    .then(answers => doCheckout(answers.branch));
+    .then(answers => doCheckout(answers.branch))
+    .catch(err => logger(err));
 }
 
 module.exports = { getBranches };
